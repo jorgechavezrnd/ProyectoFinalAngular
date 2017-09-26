f8f37d23dec4c39a42824b33e102af4da6994b2f
Jorge <jorgechavezrnd@gmail.com>
2017-09-26 13:33:11 -0400

    ngfor funcionando

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
+  private materias:any=[];
 
-  constructor() { }
+  constructor() { 
+    this.materias.push("Taller de programacion");
+    this.materias.push("Redes de computadoras I");
+    this.materias.push("Bases de datos II");
+    this.materias.push("Sistemas web");
+    this.materias.push("Estructuras de datos y algoritmos");
+    this.materias.push("Sistemas operativos");
+    this.materias.push("Bases de datos I");
+    this.materias.push("Investigacion operativa I");
+    this.materias.push("Taller de investigacion");
+    this.materias.push("Programacion II");
+  }
 
   ngOnInit() {
   }
--- src/app/components/habilidades/habilidades.component.ts
+++ src/app/components/habilidades/habilidades.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./habilidades.component.css']
 })
 export class HabilidadesComponent implements OnInit {
+  private lenguajes:any=[];
 
-  constructor() { }
+  constructor() {
+    this.lenguajes.push("C++");
+    this.lenguajes.push("Java");
+    this.lenguajes.push("C#");
+    this.lenguajes.push("C");
+    this.lenguajes.push("Python");
+    this.lenguajes.push("PHP");
+    this.lenguajes.push("Haskell");
+    this.lenguajes.push("Scala");
+    this.lenguajes.push("Ruby");
+    this.lenguajes.push("Ensamblador");
+   }
 
   ngOnInit() {
   }
